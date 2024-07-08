caa3bfc9f7b7db02bdb1603d7fe15b6c2b6c3187
Samuel Pampolini <samuel.pampolini@emergn.com>
2024-07-08 15:04:56 +0100

    Implement pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,7 +13,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   function handleSearchCallBack(term: string) {
     const params = new URLSearchParams(searchParams);
-  
+    params.set('page', '1');
     if(term) {
       params.set('query', term);
     } else {
